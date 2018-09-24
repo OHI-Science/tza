8e2101d6ec50d5257b1e2d1ebd691531cfb365d0
mishal089 <36955559+mishal089@users.noreply.github.com>
2018-09-24 12:10:18 +0300

    updated script to clean dataset for mangrove health data layer

--- prep/HAB/Mangrove/data layers/script to calculate mangrove current status.R
+++ prep/HAB/Mangrove/data layers/script to calculate mangrove current status.R
@@ -1,9 +1,15 @@
 #Objective-Calculating mangrove current status
 
-library(dplyr)
-csv2014<-read.csv("D:/git/tza/prep/HAB/Mangrove/Extracted_regional_value _csv/TZAmangrove_2014.csv")
+library(here)
+#steps:
+#assign the correct county (maybe under sector) to each site
+setwd(here::here('prep/HAB/Mangrove/'))
 
-csv2000<-read.csv("D:/git/tza/prep/HAB/Mangrove/Extracted_regional_value _csv/TZAmangrove_2000.csv")
+
+library(dplyr)
+csv2014<-read.csv("Extracted_regional_value _csv/TZAmangrove_2014.csv")
+
+csv2000<-read.csv("Extracted_regional_value _csv/TZAmangrove_2000.csv")
 #change header name
 names(csv2000)[5]<-paste("ref year2000")
 
@@ -11,12 +17,17 @@ names(csv2000)[5]<-paste("ref year2000")
 datamerged<-merge(csv2014, csv2000, by="rgn_name")
 
 #Calculating curent status
-datamerged$C_status<-(datamerged$area_km2/datamerged$`ref year2000`)
+datamerged$health<-(datamerged$area_km2/datamerged$`ref year2000`)
 
 
-cal_status <- datamerged[c(1,2,4,10)]
-cal_status["habitat"]<-"mangrove"
+cal_status <- datamerged[c(2,3,4,10)]
+# cal_status["habitat"]<-"mangrove"
+colnames(cal_status)[1]<-'rgn_id'
+colnames(cal_status)[2]<-'habitat'
+colnames(cal_status)[3]<-'year'
+
+cal_status<-cal_status[order(cal_status$rgn_id),]
 
 #Export calculated data
 
-write.csv(cal_status,"D:/git/tza/prep/HAB/Mangrove/data layers/hab_mangrove_health_ken2018.csv")
+write.csv(cal_status,"data layers/hab_mangrove_health_tan2018.csv",row.names = F)

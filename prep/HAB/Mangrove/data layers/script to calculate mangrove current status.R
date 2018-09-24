c754042f02ce8bd3e31d75453daa0522d2eb072d
mishal089 <36955559+mishal089@users.noreply.github.com>
2018-09-24 12:26:20 +0300

    produced mangrove extent layer with data for all years and updated script to produce this output

--- prep/HAB/Mangrove/data layers/script to calculate mangrove current status.R
+++ prep/HAB/Mangrove/data layers/script to calculate mangrove current status.R
@@ -31,3 +31,30 @@ cal_status<-cal_status[order(cal_status$rgn_id),]
 #Export calculated data
 
 write.csv(cal_status,"data layers/hab_mangrove_health_tan2018.csv",row.names = F)
+
+
+
+# merge data years to create extent layer ---------------------------------
+
+setwd(here::here('prep/HAB/Mangrove/Extracted_regional_value _csv'))
+
+#steps below are to read in all mangrove extent files for each year and combine them into single object
+f <- list.files(pattern="TZA")     #lists all files in target folder with .tif in file name
+
+for (i in 1:length(f)){
+ g<- read.csv(f[i],header = T,stringsAsFactors = F)
+
+ if (i==1){
+ g1<-g}else{
+ g1<-rbind(g1,g)}
+
+
+}
+
+final<-g1[,c(1,3,4,5)]
+
+colnames(final)[4]<-'km2'
+
+final<-final[order(final$rgn_id),]
+
+write.csv(final,"hab_mangrove_extent_tan2018.csv",row.names = F)
